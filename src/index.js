ba0bab936500751cc0cb48f82e70c92617ec7dcf
Martin Barker <martinbarker99@gmail.com>
2020-12-06 20:56:29 -0800

    buttonBgcolor

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+$(document).keyup(function(e) {
+    if (e.key === "Escape") { 
+        if($('#uploadModal').is(':visible')){
+           $('#uploadModal').modal('toggle');
+        }
+       
+   }
+});
+$(document).ready(function () {
+    
+    /*
+    $('#example').dataTable({
+        "processing": true, // control the processing indicator.
+        "serverSide": true, // recommended to use serverSide when data is more than 10000 rows for performance reasons
+        "info": true,   // control table information display field
+        "stateSave": true,  //restore table state on page reload,
+        "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],    // use the first inner array as the page length values and the second inner array as the displayed options
+        "ajax":{
+            "url": "@string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Authority, Url.Content("~"))/Home/AjaxGetJsonData",
+            "type": "GET"
+        },
+        "columns": [
+            { "data": "Name", "orderable" : true },
+            { "data": "Age", "orderable": false },
+            { "data": "DoB", "orderable": true },
+            {
+                "render": function (data, type, JsonResultRow, meta) {
+                    return '<img src="Content/'+JsonResultRow.ImageAddress+'">';
+                }
+            }
+        ],
+        "order": [[0, "asc"]]
+    });
+    */
+});
 
 let dropArea = document.getElementById('newUploadFilesInput');
 ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
@@ -34,8 +69,7 @@ function handleDrop(e) {
 function handleFiles(files) {
     console.log('handleFiles() ', files)
     for (var fileNum = 0; fileNum < files.length; fileNum++ ) {
-        console.log('fileNum=', fileNum)
-        console.log('files[fileNum]=', files[fileNum])
+        console.log('file = ', files[fileNum])
         document.getElementById('newUploadFilesDisplay').innerHTML = 
         `
         ${document.getElementById('newUploadFilesDisplay').innerHTML} 
